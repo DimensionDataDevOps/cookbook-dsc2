7d134a7a2a223ddc1a594e99135f79ed51e278d2
Eugene Narciso <eugene.narciso@itaas.dimensiondata.com>
2016-04-25 14:15:08 -0700

    update source and issues_url one more time

--- metadata.rb
+++ metadata.rb
@@ -4,12 +4,12 @@ maintainer_email 'eugene.narciso@itaas.dimensiondata.com'
 license          'Apache 2.0'
 description      'Installs/Configures dsc'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 supports         'windows', '>= 6.3'
 
-source_url       'https://github.com/DimensionDataDevOps/dsc2' if respond_to?(:source_url)
-issues_url       'https://github.com/DimensionDataDevOps/dsc2/issues' if respond_to?(:issues_url)
+source_url       'https://github.com/DimensionDataDevOps/cookbook-dsc2' if respond_to?(:source_url)
+issues_url       'https://github.com/DimensionDataDevOps/cookbook-dsc2/issues' if respond_to?(:issues_url)
 
 depends          'windows', '~> 1.39.2'
 depends          'powershell', '~> 3.2.3'
